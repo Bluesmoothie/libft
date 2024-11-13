9c63e262f359dc82c92bb9276906d23238ff23f6
Yves Gille <ygille@student.42lyon.fr>
2024-11-13 14:17:17 +0100

    Ajout de ft_atoi

--- libft.h
+++ libft.h
@@ -6,14 +6,17 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:04:31 by ygille            #+#    #+#             */
-/*   Updated: 2024/11/13 13:32:04 by ygille           ###   ########.fr       */
+/*   Updated: 2024/11/13 14:07:26 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# define MAX_INT	2147483647
+
 # include <stddef.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -28,6 +31,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 void	ft_bzero(void *s, size_t n);
+int		ft_atoi(const char *nptr);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -40,5 +44,7 @@ int		ft_tolower(int c);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
