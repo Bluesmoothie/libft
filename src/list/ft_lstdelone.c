8ed2b3ef83b67f36163a7c3cc47d4939a6a933b9
Yves Gille <ygille@student.42lyon.fr>
2025-04-13 00:21:41 +0200

    Little protection in lstdelone

--- src/list/ft_lstdelone.c
+++ src/list/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 12:22:10 by ygille            #+#    #+#             */
-/*   Updated: 2025/02/07 16:54:18 by ygille           ###   ########.fr       */
+/*   Updated: 2025/04/13 00:21:18 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (lst != NULL)
 	{
-		del (lst->content);
+		if (lst->content != NULL)
+			del (lst->content);
 		free(lst);
 	}
 }
